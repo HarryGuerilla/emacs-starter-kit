ac2b1f980b7438fb10bf77466e88f1624fd36908
Tony Pelaez <tony@tony-pelaezs-macbook-3.local>
2009-09-19 13:59:12 -0700

    mapped mac command key to meta key
    added mac system fonts to terminal

---
+++ tony.el
@@ -0,0 +1,7 @@
+;; SET COMMAND KEY TO META KEY
+(setq mac-command-modifier 'meta)
+
+;; SET MAC SYSTEM FONTS FOR TERMINAL
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
