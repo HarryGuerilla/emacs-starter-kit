dac43e4c26c11606e2d063a3ef1310c93aca0571
Tony Pelaez <tony@tony-pelaezs-macbook-3.local>
2009-09-22 12:44:58 -0700

    set functionality enabling variables in custom config file

--- tony.el
+++ tony.el
@@ -1,3 +1,11 @@
+;; DECIDE WHICH FUNCTIONALITY TO ENABLE
+(setq *macbook-support-enabled* t)
+(setq *spell-check-support-enabled* t)
+(setq *byte-code-cahce-enabled* t)
+(setq *is-a-mac* (eq system-type 'darwin))
+(setq *is-carbon-emacs* (and *is-a-mac* (eq window-system 'mac)))
+(setq *is-cocoa-emacs* (and *is-a-mac* (eq window-system 'ns)))
+
 ;; SET COMMAND KEY TO META KEY
 (setq mac-command-modifier 'meta)
 
