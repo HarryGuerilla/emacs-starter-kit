1387d3d72660827623d2708f59cf0e5ae9b6defc
Tony Pelaez <tony@tony-pelaezs-macbook-3.local>
2009-09-22 12:21:45 -0700

    enabled server-mode

--- tony.el
+++ tony.el
@@ -5,3 +5,9 @@
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
+
+;; ENABLE MENU-BAR FOR EMACS 23
+(menu-bar-mode)
+
+;; ENABLE EMACS SERVER
+(server-start)
