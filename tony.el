5251e5d6a2b090977db7e000a38391e0fb117487
Tony Pelaez <tony@tony-pelaezs-macbook-4.local>
2009-09-24 12:54:26 -0700

    added org-mode

--- tony.el
+++ tony.el
@@ -99,3 +99,11 @@
 ;; SET FRAME SIZE TO MAXIMUM
 (set-frame-width (selected-frame) 200)
 (set-frame-height (selected-frame) 100)
+
+;; ORG MODE
+;; The following lines are always needed.  Choose your own keys.
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+(setq org-hide-leading-stars t)
