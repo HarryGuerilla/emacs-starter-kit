6db53048a5554bd5c81aee86caee33c9398c3ff1
Tony Pelaez <tnyplz@gmail.com>
2010-09-03 21:38:48 -0700

    added extra stuff found on github

--- init.el
+++ init.el
@@ -2,45 +2,56 @@
 ;; ===========
 ;; basic emacs configuration for ruby on rails development
 
-;; CUSTOMIZED VARIABLES
+;; CUSTOMIZED VARIABLES =================================================
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
-;; APPEARANCE
+;; APPEARANCE ===========================================================
+
+(setq line-number-mode t)
 (global-linum-mode 1) ; show line numbers
+(display-time)        ; show clock
+
 (setq inhibit-startup-screen t)
 (setq inhibit-scratch-message nil)
-(setq font-lock-maximum-decoration t)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
+
 (show-paren-mode t) ; highlight matching paren
-(display-time)      ; show clock
+(setq show-paren-style 'mixed)
+
 (setq column-number-mode t)
-(setq line-number-mode t)
+
+(setq default-frame-alist '((font . "inconsolata")))
+(push '(font-backend xft x) default-frame-alist)
+(setq font-lock-maximum-decoration t)
 
 ;(require 'blank-mode)
 
-;(require 'highline) 
+;(require 'highline)
 ;(highline-mode 1) ; highlight current line
 
-;; GENERAL CONFIGURATION
+;; GENERAL CONFIGURATION ================================================
 '(recentf-mode t)
 '(transient-mark-mode t)
 (setq make-backup-files nil)
+(setq query-replace-highlight t)
 (setq require-final-newline t)
 (setq-default intent-tabs-mode nil)
 (server-start) ; allow access from emacs client
 (desktop-save-mode t) ; restore desktop on restart
 (setq require-final-newline t)
 (mouse-wheel-mode t)
+(setq default-directory "~/")
+(fset 'yes-or-no-p 'y-or-n-p)
+(setq major-mode 'text-mode)
 
-;; KEYBOARD SHORTCUTS
+;; KEYBOARD SHORTCUTS ===================================================
 (global-set-key "\M-z" 'undo)
 (global-set-key "\M-s" 'save-buffer)
 (global-set-key "\C-xt" 'term)
 
-
-;; RUBY
+;; RUBY =================================================================
 (setq auto-mode-alist (cons '("\\(?:\\.irbc\\|\\.rb\\)$" . ruby-mode)
 			    auto-mode-alist))
 (autoload 'run-ruby "inf-ruby"
@@ -50,3 +61,4 @@
 
 (setq load-path (cons (expand-file-name "~/.emacs.d/vendor/rails-mode") load-path))
 (require 'rails-autoload)
+(require 'inf-ruby)
