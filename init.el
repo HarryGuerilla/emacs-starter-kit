665a32163b1c3cfbcdd5f1f0ef4ebe9c3c6439e8
Tony Pelaez <tnyplz@gmail.com>
2011-02-11 22:31:14 -0800

    changed meta key for mac

--- init.el
+++ init.el
@@ -92,6 +92,12 @@
 ;; make emacs use the clipboard
 (setq x-select-enable-clipboard t)
 
+;; mac keyboard keys
+(setq mac-option-key-is-meta nil)
+(setq mac-command-key-is-meta t)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier nil)
+
 ;; ===================================================================
 ;; KEYBOARD SHORTCUTS
 ;; ===================================================================
