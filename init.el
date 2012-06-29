9881cae57bd86280da92b494b420c4fa6401be9a
Tony Pelaez <tnyplz@gmail.com>
2012-06-29 00:04:56 -0700

    updated cedet to fix error on starting emacs

--- init.el
+++ init.el
@@ -278,7 +278,7 @@
 ;; ===================================================================
 
 ;;(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/cedet-1.1/common"))
-(load-file "~/.emacs.d/site-lisp/cedet-1.0/common/cedet.el")
+(load-file "~/.emacs.d/site-lisp/cedet-1.1/common/cedet.el")
 ;;(load-file (expand-file-name "~/.emacs.d/site-lisp/cedet-1.0/common/cedet.el"))
 (global-ede-mode 1)                      ; Enable the Project management system
 (semantic-load-enable-code-helpers)      ; Enable prototype help and smart completion 
