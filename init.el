a0c770af11fa5caec441afb7ab191fc55519435e
Tony Pelaez <tnyplz@gmail.com>
2012-08-01 21:35:48 -0700

    configure haskell

--- init.el
+++ init.el
@@ -482,3 +482,14 @@
   '(progn
      (require 'js2-imenu-extras)
      (js2-imenu-extras-setup)))
+
+
+;; ===================================================================
+;; Haskell
+;; ===================================================================
+
+(load "~/.emacs.d/site-lisp/haskell-mode/haskell-site-file")
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
