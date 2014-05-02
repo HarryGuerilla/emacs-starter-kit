01e790291a6c2b70d1f9ac2dd27b573e5d72710b
Tony Pelaez <tnyplz@gmail.com>
2014-05-02 10:05:54 -0700

    added markdown mode

--- init.el
+++ init.el
@@ -515,8 +515,7 @@
 
 ;; ===================================================================
 ;; Markdown
-;; ================================================================
+;; ===================================================================
 
-(autoload 'markdown-mode "markdown-mode"
-  "Major mode for editing Markdown files" t)
+(load "~/.emacs.d/site-lisp/markdown-mode/markdown-mode.el")
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
